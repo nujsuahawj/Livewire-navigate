7fcaa9687878aea42757d09f9cab77472e2be0c9
Nujsua Hawj <nousainther@gmail.com>
2023-09-04 11:49:10 +0700

    tiav lawm test

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Livewire\PageFore;
+use App\Livewire\PageOne;
+use App\Livewire\PageThree;
+use App\Livewire\PageTwo;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,16 +17,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('page1');
-})->name('page1');
-Route::get('/page2', function () {
-    return view('page2');
-})->name('page2');
+// Route::get('/', function () {
+//     return view('page1');
+// })->name('page1');
+Route::get('/', PageOne::class)->name('page1');
 
-Route::get('/page3', function () {
-    return view('page3');
-})->name('page3');
-Route::get('/page4', function () {
-    return view('page4');
-})->name('page4');
+// Route::get('/page2', function () {
+//     return view('page2');
+// })->name('page2');
+Route::get('/page2', PageTwo::class)->name('page2');
+
+// Route::get('/page3', function () {
+//     return view('page3');
+// })->name('page3');
+Route::get('/page3', PageThree::class)->name('page3');
+
+// Route::get('/page4', function () {
+//     return view('page4');
+// })->name('page4');
+Route::get('/page4', PageFore::class)->name('page4');
